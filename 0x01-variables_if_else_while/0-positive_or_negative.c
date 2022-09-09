ff7cb2ba9ff8654a42b7ee3c7f25ecfbc063231b
Eyethu <followme30303@gmail.com>
2022-09-09 12:20:36 -0700

    Once_off

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -23,7 +23,6 @@ int n;
 			} else if (n == 0)
 			{
 				printf("%d is zero\n", n);
-
 			} else
 			{																			printf("%d is positive\n", n);			
 			}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
