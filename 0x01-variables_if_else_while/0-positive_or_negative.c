ff4caade92f7c0a9e481ebce142b1542bf084026
Eyethu <followme30303@gmail.com>
2022-09-09 06:17:02 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,7 +19,6 @@ int n;
 			if (n < 0)
 
 			{
-
 				printf("%d is negative\n", n);
 
 			} else if (n == 0)
@@ -30,10 +29,8 @@ int n;
 			} else
 		
 			{
-																						printf("%d is positive\n", n);
-
-			
+																						printf("%d is positive\n", n);			
 			}
 
-			return(0);
+			return (0);
 }
