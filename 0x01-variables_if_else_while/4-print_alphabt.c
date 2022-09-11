2e896308c97d96a04d4de9d679879652fd0875bc
Eyethu <followme30303@gmail.com>
2022-09-11 06:39:00 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-
+/**main: This funtion prints the alphabet in lowercase with exceptions
+ * description: the function prints all alphabet in lowercase
+ * except for 'q' and 'e' using the putchar() function
+ *Return: 0
+*/
 int main(void)
-
 {
-	char a;
-	for (a = 'a'; a <= 'z' ; a++)
-	{
-		if (a == 'q' || a == 'e')
-		{
-			continue;
-		}
-		putchar(a);
-	}
-	putchar('\n');
-	return (0);
+char a;
+for (a = 'a'; a <= 'z' ; a++)
+{
+if (a != 'e' || a != 'q')
+{
+	putchar(a);
+}
+}
+putchar('\n');
+return (0);
 }
