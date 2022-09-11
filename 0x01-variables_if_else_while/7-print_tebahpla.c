cdf906a61fde2d79dbca0ae0a7bf78fbf3e3069c
Eyethu <followme30303@gmail.com>
2022-09-11 07:36:13 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+/**
+ * main: this function prints the alphabet in
+ * lowercase reversed
+ * Description: each letter is displayed using putchar
+ * that is used twice
+ * Return: 0
+*/
 
 int main(void)
-
 {
-	char a;
-	for (a = 'z'; a >= 'a' ; a--)
-		putchar(a);
-	putchar('\n');
-	return (0);
+char a;
+for (a = 'z'; a >= 'a' ; a--)
+{
+	putchar(a);
+}
+putchar('\n');
+return (0);
 }
