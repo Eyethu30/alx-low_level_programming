9010d8c2221c76c64971ac58ab1b91b8428d2de2
Eyethu <followme30303@gmail.com>
2022-09-11 07:28:29 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-
+/**
+ * main: this function prints all single
+ * digit numbers starting from 0 until 10
+ * Return: 0
+*/
 int main(void)
 {
-	int a = 0;
-	for (; a < 10 ; a++)
-		putchar('0' + a);
-	putchar('\n');
-	return (0);
+int a = 0;
+for (; a < 10 ; a++)
+{
+	putchar('0' + a);
+}
+putchar('\n');
+return (0);
 }
