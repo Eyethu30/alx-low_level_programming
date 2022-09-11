e4fa1b8ef908914f57641d755467e562fb839082
Eyethu <followme30303@gmail.com>
2022-09-11 06:25:46 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 /**
  * main: prints the alphabet in lowercase and uppercase
  * Description: the program prints the alphabet in lower and uppercase
- * displays the result using the putchar() function
+ * this program displays the result using the putchar() function
  * Return: 0
  */
 int main(void)
@@ -11,11 +11,11 @@ char a;
 for (a = 'a'; a <= 'z' ; a++)
 {
 	putchar(a);
-	putchar('\n');
 }
 for (a = 'A'; a <= 'Z'; a++)
 {
 	putchar(a);
 }
+putchar('\n');
 return (0);
 }
