03d7c777a0c3cfc6b54b24b813fef31545e89fa7
Eyethu <followme30303@gmail.com>
2022-09-11 23:51:59 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+/**
+*main - is executed fisrt
+*Description - this program prints all numbers of base 16
+*followed by a new line
+*Return: 0
+*/
 int main(void)
-
 {
-
 	unsigned char a = '0';
 	int i;
 	for (i = 0; i < 10; i++)
