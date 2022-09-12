e49e2bf52f28cf304ae802a5bc6787dcb4016568
Eyethu <followme30303@gmail.com>
2022-09-12 01:18:02 -0700

    new_changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+/**
+ * main - starting point of every compilation
+ * Description: display at a statement using printf()
+ * Return: 0
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+/**
+ * main - this is the starting point of every code
+ * Description: Outputs a statement using printf()
+ * Return: 0
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
