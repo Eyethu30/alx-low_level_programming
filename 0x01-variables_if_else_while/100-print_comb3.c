bab00ee2356e44261050031fecb9b7dc4d04c0e6
Eyethu30 <111011922+Eyethu30@users.noreply.github.com>
2022-09-12 12:40:54 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 /**
  *
- * * main - prints all possible different combinations of two digits
+ * main - prints all possible different combinations of two digits
  *
- * * Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  *
- * */
+ */
 int main(void)
 {
 int n, m;
-for (n = 0; n <= 9; n++){for (m = 1; m <= 9; m++)
+for (n = 0; n <= 9; n++)
+{
+for (m = 1; m <= 9; m++)
 {
 if (m > n)
 {
