6f7d9e5e6d2a481d2764aea6b07605106031bca8
Eyethu <followme30303@gmail.com>
2022-09-12 00:49:04 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-/**main - prints all possible combinations of single digit numbers
+/*
+*main - prints all possible combinations of single digit numbers
 *Description: Numbers must are separated by ,, followed by a space
 *Numbers are printed in ascending order
 */
@@ -10,13 +11,12 @@ int i;
 for (i = 10; i < 20; i++)
 {
 	putchar((i % 10) + '0');
-}
 if (i != 19)
 {
 putchar(',');
 putchar(' ');
 }
+}
 putchar('\n');
 return (0);
 }
-
