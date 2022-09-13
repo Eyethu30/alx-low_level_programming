7ce68d77064432d51fd08a8fbb27cdd39c0da367
Eyethu <followme30303@gmail.com>
2022-09-13 04:31:26 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the starting point of code 
+ * Description: print _putchar add a new line after
+ * Return: 0
+ */
+char word;
+int main(void)
+{
+putchar(word);
+putchar('\n');
+return (0);
+}
