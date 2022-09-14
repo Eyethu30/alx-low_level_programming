b4b4d0df236f677f140238f5af283d7b4b8f98d4
Eyethu <followme30303@gmail.com>
2022-09-13 22:52:20 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+#include "main.h"
+#include <string.h>
 /**
- * main - the starting point of code 
+ * main - the starting point of code
  * Description: print _putchar add a new line after
  * Return: 0
  */
-char word;
+
+char s[] = "_putchar";
+char i;
 int main(void)
 {
-putchar(word);
-putchar('\n');
+for (s[0-9];;)
+{
+	putchar(s[i]);
+	i++;
+}
 return (0);
 }
