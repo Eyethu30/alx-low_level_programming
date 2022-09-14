6effd380678af87aa381e683974e10c13f1b3269
Eyethu <followme30303@gmail.com>
2022-09-14 05:45:50 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - main function
+ * @c : charc comp
+ * return 1 for true and 0 for false
+ *
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+
+	return(1);
+}
+return (0);
+}
