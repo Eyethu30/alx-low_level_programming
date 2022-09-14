7e714586a8a584192733f6d64060f94a5efebb63
Eyethu <followme30303@gmail.com>
2022-09-14 00:21:17 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - the start of every code 
+ * Description: this program prints the alphabet in lowercase
+ * Returns: 0
+ */
+
+int main(void)
+{
+
+void print_alphabet(void);
+{
+char alpha;
+alpha = 'a';
+for (alpha = 'a'; alpha = 'z'; alpha++)
+{
+	putchar(alpha);
+}
+}
+putchar('\n');
+return (0);
+}
