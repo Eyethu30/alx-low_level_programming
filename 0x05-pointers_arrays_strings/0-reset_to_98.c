e735c44b2c8634f62ae07f04c586c6dbb3b6c209
Eyethu <followme30303@gmail.com>
2022-09-19 14:36:20 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - This function writes a pointer that
  * void reset_to_98(int *n); This function takes a pointer as a parameter
  * Return: 0 
  * @n: int type variable
  */
-
-
 void reset_to_98(int *n)
 {
 *n = 98;
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,11 @@
 #include "main.h"
-
 /**
  * void swap_int(int *a, int *b) - the main funtion.
  * @a - swap pointer
  * @b - swap pointer
  */
-
 void swap_int(int *a, int *b);
 {
 	*a = *b;
 	*b = *a;
-
 }
