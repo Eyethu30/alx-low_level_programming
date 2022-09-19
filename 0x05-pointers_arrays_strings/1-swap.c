e670e1ac81d455f28ebfba6bdd05f25fb87dfab6
Eyethu <followme30303@gmail.com>
2022-09-19 14:32:48 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * void swap_int(int *a, int *b) - the main funtion.
+ * @a - swap pointer
+ * @b - swap pointer
+ */
+
+void swap_int(int *a, int *b);
+{
+	*a = *b;
+	*b = *a;
+
+}
