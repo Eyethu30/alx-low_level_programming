397e639e03c320796d947905a6e81894f5427aff
Eyethu30 <111011922+Eyethu30@users.noreply.github.com>
2022-09-20 22:33:12 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
 /**
- *
- *
- *
+ * void _puts(): - main funtion that displays string
+ * puts: - funtion used to display to the stdout
+ * @str: - variable containing string
  */
+
+void _puts();
+{
+  puts(str);
+}
