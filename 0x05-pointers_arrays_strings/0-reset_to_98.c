c1f20d724f2e182178b1fe4c948600c45853960a
Eyethu30 <111011922+Eyethu30@users.noreply.github.com>
2022-09-20 22:08:58 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main: This function writes a pointer that
- * reset_to_98 - This function takes a pointer as a parameter
- * Return: 0
+ * reset_to_98 - This is the main function
  * @n: - int type variable
  */
 void reset_to_98(int *n)
