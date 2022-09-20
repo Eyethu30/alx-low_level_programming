b1302f1700dfab877095f6861617d7a5dbd27622
Eyethu30 <111011922+Eyethu30@users.noreply.github.com>
2022-09-20 22:02:37 +0200

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 /**
-*swap_int - funtion that swaps two intergers
-* @a - first int
-* @b - second int
-*/
+ * swap_int - funtion that swaps two intergers
+ * @a - first int
+ * @b - second int
+ */
 void swap_int(int *a, int *b)
 {
 int var1 = *a;
