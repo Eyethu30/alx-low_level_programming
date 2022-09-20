ea91ca670fc17bf200092923340d60b218597ce3
Eyethu30 <111011922+Eyethu30@users.noreply.github.com>
2022-09-20 21:53:36 +0200

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * '_strlen' - returns the length of a string.
- * 's' - is a pointer variable.
+ * _strlen - returns the length of a string using '_strlen'.
+ * s - is a pointer variable 's'.
  * return - the funtion will return the length of the len string.
  */
 
