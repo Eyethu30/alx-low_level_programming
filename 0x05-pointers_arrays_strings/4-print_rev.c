464036d06daf8843298a2227a7e1afa91c086931
Eyethu30 <111011922+Eyethu30@users.noreply.github.com>
2022-09-20 23:50:31 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,22 @@
-#include <string.h>
 #include "main.h"
 /**
- * print_rev - prints reversed string
- * @s: pointer variable
+ * print_rev - prints a string reversed 
+ * @s: str input
+ * Return: reverse string
  */
-
 void print_rev(char *s)
 {
-  strrev();
+int fcounter = 0;
+int i;
+int n;
+for (i = 0; s[i] != '\0'; i++)
+{
+fcounter++;
+}
+
+for (n = (fcounter - 1); n >= 0; n--)
+{
+puts(s[n]);
+}
+puts('\n');
 }
