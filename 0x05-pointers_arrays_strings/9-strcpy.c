405039855955cfa0ba6b31c83bff3a8b72759624
Eyethu30 <111011922+Eyethu30@users.noreply.github.com>
2022-09-21 05:20:22 +0200

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - copies string from src
+ * @dest: copy
+ * @src: copy 
+ * Return: a string
+ */
+char *_strcpy(char *dest, char *src)
+{
+int ss = 0;
+while (*(src + ss) != '\0')
+{
+  *(dest + ss) = *(src + ss);
+  i++;
+}
+  *(dest + ss) = '\0';
+  
+return (dest);
+}
