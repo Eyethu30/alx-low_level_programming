b20843266b4bd8168a5b68fa572769b30e1783dd
Eyethu30 <111011922+Eyethu30@users.noreply.github.com>
2022-09-21 04:53:13 +0200

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,20 @@
-#include <>
+#include "main.h"
+
+/**
+ * puts2 - function should display the other haracter
+ * from the first
+ * @str: the input received
+ */
+void puts2(char *str)
+{
+int i = 0;
+
+for (; str[i] != '\0'; i++)
+{
+if ((i % 2) == 0)
+_putchar(str[i]);
+else
+continue;
+}
+_putchar('\n');
+}
