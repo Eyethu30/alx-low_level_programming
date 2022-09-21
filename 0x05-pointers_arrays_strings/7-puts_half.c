31a7e99ba0f872033cdb38dd68fb2a81954f954c
Eyethu30 <111011922+Eyethu30@users.noreply.github.com>
2022-09-21 04:58:04 +0200

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,23 @@
-#include
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_half - should print half the second string
+ * @str: The input
+ * Return: half of input
+ */
+
+void puts_half(char *str)
+{
+int i;
+int num;
+int len;
+len = 0;
+for (i = 0; str[i] != '\0'; i++)
+len++;
+num = (len / 2);
+if ((len % 2) == 1)
+num = ((len + 1) / 2);
+for (i = num; str[i] != '\0'; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
