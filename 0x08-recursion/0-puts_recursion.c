a1a942a3a7cb396ece8176cf66f21491dc392e81
Eyethu <followme303030@gmail.com>
2022-09-28 12:18:12 +0200

    editing_funtion__puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,17 +4,8 @@
 * @s: string paramenter
 */
 
-int main ()
+void _puts_recursion(char *s) /*This is a funtion declaration*/
 {
-void _puts_recursion(char *s)
-{
-if (*s == '\0')
-{
-_putchar('\n');
-return;
-}
-_putchar(*s);
-s++;
-_puts_recursion(s);
-}
+    _putchar(*s); /*This is supposed to print the string pointed to by s*/
+_putchar('\n'); /*This is a new line*/
 }
