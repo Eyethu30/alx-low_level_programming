44cce6bfd05ed91974f82167e80cd9c46b3e1062
Eyethu <followme303030@gmail.com>
2022-09-28 23:18:11 +0200

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include "string.h"
+
+/**
+ * _print_rev_recursion: this is the function name.
+ * the_len: variable storing length.
+ * @s: this is the argument.
+ */
+
+int _strlen_recursion(char *s)
+{
+    char i;
+    int the_len;
+    char *s[];
+    for (i = 0, i != '\0', i++)
+    {
+        _putchar(s[i]);
+        
+    }
+}
