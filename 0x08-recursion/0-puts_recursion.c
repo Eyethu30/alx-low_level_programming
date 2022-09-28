fc7810941fc19c5fc71d44a1a10941693912b732
Eyethu <followme303030@gmail.com>
2022-09-28 21:54:01 +0200

    new_commits

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 #include "main.h"
-#include <string.h>
-#include "_putchar.c"
-
 /**
 * _puts_recursion -> this is a function to print string recursivly
 * @s: string paramenter
 */
-void new_char(char *s)
-{
-    write(1, &, 1);
-}
-
 void _puts_recursion(char *s)
-
-{ 
-if(char *s != '\0')
 {
-    
-    new_char(*s);
-    new_char('\n');
 
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
 }
+
+_putchar(*s);
+
+s++;
+
+_puts_recursion(s);
 }
