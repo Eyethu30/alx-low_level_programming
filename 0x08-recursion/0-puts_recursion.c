735a54536e8110b9eba607a83eb31de5a4dddb53
Eyethu <followme303030@gmail.com>
2022-09-28 11:33:45 +0200

    minor_changes_to_library

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
-#include main.h
 #include <stdio.h>
+#include main.h
 
 /**
  * @s: the variable containing the string
@@ -7,7 +7,7 @@
  * return: void
  */
 
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
     char *s = "This is a string";
     puts(*s);
