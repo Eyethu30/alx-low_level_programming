66a123726a8ea518b1b0085d00cc86c6b6c7fdb7
Eyethu <followme303030@gmail.com>
2022-09-28 12:26:15 +0200

    new_changes

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,12 +3,9 @@
 * _puts_recursion -> this is a function to print string recursivly
 * @s: string paramenter
 */
-int main()
-{
+
 void _puts_recursion() /*This is a funtion declaration*/
 {
     puts(*s); /*This is supposed to print the string pointed to by s*/
 puts('\n'); /*This is a new line*/
-return(0);
-}
 }
