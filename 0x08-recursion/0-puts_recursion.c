33048a38b2f8e299567052f8616ffe6edf543b53
Eyethu <followme303030@gmail.com>
2022-09-28 21:47:03 +0200

    new_message

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,8 +7,19 @@
 * _puts_recursion -> this is a function to print string recursivly
 * @s: string paramenter
 */
+void new_char(char *s)
+{
+    write(1, &, 1);
+}
+
 void _puts_recursion(char *s)
+
 { 
-    _putchar(char *s);
-    _putchar('\n');
+if(char *s != '\0')
+{
+    
+    new_char(*s);
+    new_char('\n');
+
+}
 }
