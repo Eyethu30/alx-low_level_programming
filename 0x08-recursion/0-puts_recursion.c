e509a18342b8dee3040f85f985193f5360762759
Eyethu <followme303030@gmail.com>
2022-09-28 11:26:11 +0200

    adding_comments

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,12 @@
 #include main.h
 #include <stdio.h>
 
+/**
+ * @s: the variable containing the string
+ *  _puts_recursion : the funtion name 
+ * return: void
+ */
+
 void _puts_recursion(char *s);
 {
     char *s = "This is a string";
