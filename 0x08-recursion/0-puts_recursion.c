5e73ccf454a056e18f2463ed6b4c86305e886ea4
Eyethu <followme303030@gmail.com>
2022-09-28 12:01:08 +0200

    adding_new_changes

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,9 +5,16 @@
  *  _puts_recursion : the funtion name 
  * return: void
  */
+void _puts_recursion(char *s)
 
-void _puts_recursion(char *s) 
 {
-    _putchar("%c", *s);
-    _putchar('\n');
+if (*s == '\0')
+{
+_putchar('\n');
+}
+else
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
 }
