2a0d519a5195e7990f1750b9d0d29ca2fedfa1fd
Eyethu <followme303030@gmail.com>
2022-09-28 12:20:31 +0200

    new_changes

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,6 +6,6 @@
 
 void _puts_recursion(char *s) /*This is a funtion declaration*/
 {
-    _putchar(*s); /*This is supposed to print the string pointed to by s*/
-_putchar('\n'); /*This is a new line*/
+    puts(*s); /*This is supposed to print the string pointed to by s*/
+puts('\n'); /*This is a new line*/
 }
