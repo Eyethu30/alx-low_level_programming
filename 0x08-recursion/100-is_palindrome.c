412aa857d86db878ec046c41260dff6c2374b12b
Eyethu <followme303030@gmail.com>
2022-09-29 22:01:22 +0200

    new

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,27 +1,30 @@
 #include "main.h"
+
 /**
  *_strlen_recursion - returns the length of a string
  * @s: the string
- * Return: int/ length 
+ * Return: int/ length of string
  */
 
 int _strlen_recursion(char *s)
 {
-int len = 0;
-if (*s == '\0')
-{
-return (0);
+	int len = 0;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	if (*s != '\0')
+	{
+		len++;
+		len += _strlen_recursion(++s);
+		return (len);
+	}
+	return (0);
 }
-if (*s != '\0')
-{
-len++;
-len += _strlen_recursion(++s);
-return (len);
-}
-return (0);
-}
+
 /**
-* _compareends - checks for at either end of a str
+* _compareends - checks for matches at either end of a str
 * @s: the string
 * @begin: the start
 * @end: the end
@@ -30,25 +33,28 @@ return (0);
 
 int _compareends(char *s, int begin, int end)
 {
-if (begin >= end)
-return (1);
-if (s[begin] == s[end])
-return (_compareends(s, (begin + 1), (end - 1)));
-if (s[begin] == s[end] && begin == end)
-return (1);
-return (0);
+	if (begin >= end)
+		return (1);
+	if (s[begin] == s[end])
+		return (_compareends(s, (begin + 1), (end - 1)));
+	if (s[begin] == s[end] && begin == end)
+		return (1);
+	return (0);
 }
+
 /**
  * is_palindrome - checking for palindrome
  * @s: the string
  * Return: 1 if true, 0 if not
  */
+
 int is_palindrome(char *s)
 {
-int len;
-len = _strlen_recursion(s);
-if (len == 0 || len == 1)
-return (1);
-else
-return (_compareends(s, 0, len - 1));
+	int len;
+
+	len = _strlen_recursion(s);
+	if (len == 0 || len == 1)
+		return (1);
+	else
+		return (_compareends(s, 0, len - 1));
 }
