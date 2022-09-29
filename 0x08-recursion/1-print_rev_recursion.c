3ee9288fcd087cd2afc042e8c895e84b65ed60eb
Eyethu <followme303030@gmail.com>
2022-09-29 21:55:14 +0200

    new

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,11 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (!*s)
-		return;
-	if (*s)
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+if (!*s)
+return;
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
 }
