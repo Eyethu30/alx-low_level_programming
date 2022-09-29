0a04318900217e21f35dd253ccc7c6fdc59ea185
Eyethu <followme303030@gmail.com>
2022-09-29 21:45:08 +0200

    new

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,18 +6,18 @@
  * Return: int/ length of string
  */
 
-int _strlen_recursion(char *s)
+int _strlen_recursion(char *s
 {
-	int len = 0;
-
-	if (*s == '\0')
-	{
-		return (0);
-	}
-	if (*s != '\0')
-	{
-		len++;
-		len += _strlen_recursion(++s);
-		return (len);
-	}
-	return (0);
+int len = 0;
+if (*s == '\0')
+{
+return (0);
+}
+if (*s != '\0')
+{
+len++;
+len += _strlen_recursion(++s);
+return (len);
+}
+return (0);
+}
