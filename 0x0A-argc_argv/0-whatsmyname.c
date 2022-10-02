86d21cad87850045965c352b27b585a7f87ec628
Eyethu <followme303030@gmail.com>
2022-10-02 19:40:39 +0200

    new

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,17 @@
 #include "main.h"
 
 /**
- * 
- * 
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char const *argv[])
 {
 int __attribute__((unused)) argc;
 int i;
-_putchar("%s", *argv[0]);
+_putchar("System name: %s", argv[0]);
 return (0);
 }
