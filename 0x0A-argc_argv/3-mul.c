9d4df902e4d14b16ca50739dd20333290b12f6d6
Eyethu <followme303030@gmail.com>
2022-10-02 21:41:17 +0200

    new

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,13 @@
  * Return: 0 (Success), 1 (Error)
  */
 
-int main(int argc __attribute__((unused)), char* argv[])
+int main(int argc, char* argv[])
 {
 for(,argc > 2,)
 {
-    duoble result;
     scanf("%d",*argv[1]);
     scanf("%d",*argv[2]);
-    result = *argv[1] * *argv[2];
+    result = argv[1]*argv[2];
     printf("%d\n", result);
 }
 else
