7463c6d277c00e6732bd42e35b8c0a8555f9d72f
Eyethu <followme303030@gmail.com>
2022-10-02 21:23:54 +0200

    new

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success), 1 (Error)
+ */
+
+int main(int argc, char* argv[])
+{
+for(,argc > 2,)
+    result = argv[1]*argv[2];
+    printf("%i\n", result);
+}
+else
+{
+    printf("Error");
+    return (1);
+}
