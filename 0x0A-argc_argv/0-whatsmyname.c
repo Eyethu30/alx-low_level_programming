39f462bea3682932a681a4b57af25c9b839b09ac
Eyethu <followme303030@gmail.com>
2022-10-02 19:30:22 +0200

    new

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * 
+ * 
+ */
+
+int main(int argc, char const *argv[])
+{
+int __attribute__((unused)) argc;
+int i;
+_putchar("%s", *argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is a Read me file that is soon to be updated.
