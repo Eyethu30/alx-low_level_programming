a9f1e71e110b752e130aa65a1e4a0e318ef74666
Eyethu <followme303030@gmail.com>
2022-10-10 17:40:30 +0200

    new

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,8 +2,14 @@
 #define MSTRING 2-main.c
 #include <stdio.h>
 #include "main.h"
+/**
+ * main - the main function
+ * Return 0 
+ * 
+*/
 int main(void)
 
 {
     _putchar("%s\n", MSTRING);
+    return(0);
 }
