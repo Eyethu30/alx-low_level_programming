3e093d01ce067ca2e3c89c8c6b9b7bd9a31cecbe
Eyethu <followme303030@gmail.com>
2022-10-25 11:26:52 +0200

    renaming header file

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,29 @@
+#ifndef LIST_
+#define List_
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+
+#endif
