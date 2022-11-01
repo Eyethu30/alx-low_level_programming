27e2170b9ecdc0d33a8d79b0c4440e54dde99fdd
Eyethu <followme303030@gmail.com>
2022-11-01 14:56:21 +0200

    new

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,39 +2,36 @@
 #include <stdio.h>
 /**
  * create_file - creates a file
- * @filename: filename.
+ * @filename: name of the file.
  * @text_content: content writed in the file.
  * Return: 1 if it success. -1 if it fails.
  */
-
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
-    char nt;
-    int numLet;
-    if (filename == NULL)
-    {
-        return (-1);
-    }
-    if (text_content == NULL)
-    {
-        create(filename, O_RDWR);
-    }
-    if(filename)
-        if(text_content)
-        {
-            file *fn;
-            open(filename, O_RDWR);
-            nt = text_content + '/0';
-            for (numLet = 0, text_content[numLet], numLet++)
-            {
-                written = write(fn, nt, numLet);
-            }
-            if (written == -1)
-            {
-                return (-1);
-            }
-        }
-        close(fn);
+	int fd;
+	int nletters;
+	int written;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	writtten = write(fd, text_content, nletters);
+
+	if (written == -1)
+		return (-1);
+
+	close(fd);
 
 	return (1);
 }
