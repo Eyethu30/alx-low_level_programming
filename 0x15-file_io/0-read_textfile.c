82b1a22fef496b71b2db7b3dda22f47f6273851a
Eyethu <followme303030@gmail.com>
2022-11-01 12:44:15 +0200

    task  0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fn;
+	ssize_t num, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fn = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, num);
+
+	close(fn);
+
+	free(buf);
+
+	return (nwr);
+
+}
